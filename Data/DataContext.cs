846fffb7e016f43b3175e80ad4ee7b12ec549f34
Danieleg <daniel.krusinski@nexteer.com>
2023-07-05 14:43:58 +0200

    Initial commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NexteerNewsAPI.Models;
+
+namespace NexteerNewsAPI.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<CompanyNews> News => Set<CompanyNews>();
+
+    }
+}
---
+++ Models/CompanyNews.cs
@@ -0,0 +1,11 @@
+﻿namespace NexteerNewsAPI.Models
+{
+    public class CompanyNews
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
