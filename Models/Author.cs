b6109b733ad67dab49aee393df198087bc36f7f1
Danieleg <daniel.krusinski@nexteer.com>
2023-07-13 12:32:19 +0200

    Add Dto class & decorators

---
+++ Models/Author.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace CompanyNewsAPI.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Team { get; set; }
+        [JsonIgnore]
+        public ICollection<Post>? Posts { get; set; }
+    }
+}
