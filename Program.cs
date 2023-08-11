ae27deaa66712f4849d1f406a88c23c9b9e9ee2b
Danieleg <daniel.krusinski@nexteer.com>
2023-08-11 12:19:50 +0200

    add Services w config

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
 using CompanyNewsAPI.Data;
 using CompanyNewsAPI.Interfaces;
 using CompanyNewsAPI.Repositories;
-using Microsoft.AspNetCore.Builder;
+using CompanyNewsAPI.Services;
 using Microsoft.EntityFrameworkCore;
+using Hangfire;
+using CompanyNewsAPI.Helpers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 namespace CompanyNewsAPI
 {
@@ -15,6 +22,36 @@ namespace CompanyNewsAPI
             builder.Services.AddTransient<IUserRepo, UserRepo>();
             builder.Services.AddTransient<IPostRepo, PostRepo>();
             builder.Services.AddTransient<IAuthRepo, AuthRepo>();
+            builder.Services.AddTransient<IUIRepo, UIRepo>();
+
+            builder.Services.AddSingleton(provider =>
+            {
+                var email = builder.Configuration.GetValue<string>("EmailService:email");
+                var password = builder.Configuration.GetValue<string>("EmailService:password");
+                return new EmailService(email, password);
+            });
+
+
+            builder.Services.AddHangfire(options =>
+            {
+                options.UseSqlServerStorage(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+            builder.Services.AddHangfireServer();
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration.GetValue<string>("Jwt:Issuer"),
+                    ValidAudience = builder.Configuration.GetValue<string>("Jwt:Audience"),
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("Jwt:Key")))
+                };
+                options.SaveToken = true;
+            });
 
             builder.Services.AddControllers();
             builder.Services.AddCors(options => options.AddPolicy(name: "CompanyNewsUI",
@@ -29,6 +66,21 @@ namespace CompanyNewsAPI
             builder.Services.AddSwaggerGen();
             //builder.Services.AddHealthChecks();
 
+
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Description = "Standard Authorization header using the Bearer scheme (\"bearer {token}\")",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                options.OperationFilter<SecurityRequirementsOperationFilter>();
+            });
+
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -38,16 +90,25 @@ namespace CompanyNewsAPI
                 app.UseSwaggerUI();
             }
 
+            app.UseHangfireDashboard();
+
             app.UseCors("CompanyNewsUI");
 
             app.UseHttpsRedirection();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
             //app.UseEndpoints(endpoints =>
             //{
-            //    endpoints.MapHealthChecks("/health");
+            //    //endpoints.MapHealthChecks("/health");
             //});
+
+            RecurringJob.AddOrUpdate<ExpiredDataCleaner>("clean-expired-keys", job => job.CleanExpiredData(@"keys.json"), Cron.Hourly());
+            RecurringJob.AddOrUpdate<ExpiredDataCleaner>("clean-expired-errors", job => job.CleanExpiredData(@"errors.json"), Cron.Minutely());
+
+            app.UseMiddleware<ExceptionHandlerService>();
 
             app.MapControllers();
 
@@ -57,5 +118,5 @@ namespace CompanyNewsAPI
         }
 
     }
-    
+
 }
---
+++ Services/ExceptionHandlerService.cs
@@ -0,0 +1,31 @@
+﻿using CompanyNewsAPI.Models;
+using System.Text.Json;
+
+namespace CompanyNewsAPI.Services
+{
+    public class ExceptionHandlerService
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlerService> _logger;
+
+        public ExceptionHandlerService(RequestDelegate next, ILogger<ExceptionHandlerService> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                Error errorData = new Error { Message = e.ToString() };
+                FileService.AppendAllText(@"errors.json", "\n" + JsonSerializer.Serialize(errorData) + ",");
+            }
+        }
+    }
+}
