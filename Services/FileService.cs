ca068c52c18a8004437cb715cda8dc98c0b40e83
Danieleg <daniel.krusinski@nexteer.com>
2023-08-25 09:03:55 +0200

    Add Async methods

--- Services/FileService.cs
+++ Services/FileService.cs
@@ -4,25 +4,60 @@
     {
         private static Semaphore _fileSemaphore = new Semaphore(1, 1);
 
-        public static string[] ReadAllLines(string path)
+        public async static Task<string[]> ReadAllLinesAsync(string path)
         {
             try
             {
                 _fileSemaphore.WaitOne();
-                return File.ReadAllLines(path);
+                return await File.ReadAllLinesAsync(path);
             }
             finally
             {
                 _fileSemaphore.Release();
             }
         }
-
-        public static void AppendAllText(string path, string data)
+        public static string[] ReadAllLines(string path)
         {
             try
             {
                 _fileSemaphore.WaitOne();
-                File.AppendAllText(path, data);
+                return  File.ReadAllLines(path);
+            }
+            finally
+            {
+                _fileSemaphore.Release();
+            }
+        }
+        public async static Task<string> ReadFileAsync(string path)
+        {
+            try
+            {
+                _fileSemaphore.WaitOne();
+                return await File.ReadAllTextAsync(path);
+            }
+            finally
+            {
+                _fileSemaphore.Release();
+            }
+        }
+        public async static Task AppendAllTextAsync(string path, string data)
+        {
+            try
+            {
+                _fileSemaphore.WaitOne();
+                File.AppendAllTextAsync(path, data);
+            }
+            finally
+            {
+                _fileSemaphore.Release();
+            }
+        }
+        public async static Task WriteAllLinesAsync(string path, List<string> data)
+        {
+            try
+            {
+                _fileSemaphore.WaitOne();
+                File.WriteAllLinesAsync(path, data);
             }
             finally
             {
