398d1f607d076364e621051d927ffbb9fd582c02
Danieleg <daniel.krusinski@nexteer.com>
2023-08-25 08:22:53 +0200

    tweak key generator

--- Helpers/KeyGenerator.cs
+++ Helpers/KeyGenerator.cs
@@ -1,4 +1,6 @@
-﻿namespace CompanyNewsAPI.Generators
+﻿using CompanyNewsAPI.Services;
+
+namespace CompanyNewsAPI.Generators
 {
     public class KeyGenerator
     {
@@ -6,8 +8,9 @@
         private static readonly string _bigChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         private static readonly string _numbers = "0123456789";
         private static readonly string _specialChars = "!@#$%^&*()";
+        private static readonly string _path = @"keys.json";
 
-        public static string RandomStr(int length)
+        public async static Task<string> RandomStr(int length)
         {
             var everyChar = _smallChars + _bigChars + _numbers + _specialChars;
             List<char> chars = new List<char>(everyChar);
@@ -17,6 +20,12 @@
             for (int i = 0; i < length; i++)
             {
                 randomString += chars[random.Next(chars.Count - 1)];
+            }
+
+            var existingKeys = await FileService.ReadFileAsync(_path);
+            if (existingKeys.Contains(randomString))
+            {
+                return await RandomStr(length);
             }
 
             return randomString;
