997df31d7baceeea3aa15f053e4272a5a337f0e1
Danieleg <daniel.krusinski@nexteer.com>
2023-10-11 08:48:48 +0200

    add hashing with salt for passwords

---
+++ CompanyNewsAPI/Helpers/SecurityGenerators.cs
@@ -0,0 +1,49 @@
+﻿using CompanyNewsAPI.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CompanyNewsAPI.Helpers
+{
+    public class SecurityGenerators
+    {
+        public static string GenerateJSONWebToken(Login loginData, IConfiguration configuration)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetValue<string>("Jwt:Key")));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Email, loginData.Email),
+                new Claim(ClaimTypes.Role, "user")
+             };
+
+            var token = new JwtSecurityToken(
+                issuer: configuration.GetValue<string>("Jwt:Issuer"),
+                audience: configuration.GetValue<string>("Jwt:Audience"),
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        public static string ComputeSha256Hash(string password)
+        {
+
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] dataBytes = Encoding.UTF8.GetBytes(password);
+                byte[] hashBytes = sha256Hash.ComputeHash(dataBytes);
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    builder.Append(hashBytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
